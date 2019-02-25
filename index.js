654f079541704db951616ca8cfa2806351159fdb
Nicolás Aldana <gcn.ne6@gmail.com>
2019-02-25 00:30:00 -0500

    Mongoose: Database connection and load mongoose modules

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var express = require('express'),
   app = express(),
+  mongoose = require('mongoose'),
   port = process.env.PORT || 3000;
+
+var Hat = require('./api/models/hat');
+var User = require('./api/models/user');
+
+mongoose.connect('mongodb+srv://root:O7DgnKqSqCFmvC6n@tests-0eeni.mongodb.net/test');
 
 app.listen(port);
 
